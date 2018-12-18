524f31838c16e3f3a364718386348081d70d51a0
Benjamin Braud <benjamin.braud@cbp-group.com>
2018-12-18 11:20:43 +0100

    Test

---
+++ services/src/main/java/com/cbp/disponibilite/gestionnaires/services/resources/SupervisionResource.java
@@ -0,0 +1,17 @@
+package com.cbp.disponibilite.gestionnaires.services.resources;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("/private/open")
+public class SupervisionResource {
+
+    @GET
+    @Path("/dispo")
+    public Response dispo() {
+        return Response.status(200).build();
+    }
+
+}
