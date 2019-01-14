8eb2b03b8323f13e5db5dc94118b63b588f8c00b
Benjamin Braud <benjamin.braud@cbp-group.com>
2019-01-14 16:41:19 +0100

    CDCG-1099: ajout logback

--- business/src/main/java/com/cbp/disponibilite/gestionnaires/manager/GestionnairesService.java
+++ business/src/main/java/com/cbp/disponibilite/gestionnaires/manager/GestionnairesService.java
@@ -2,6 +2,8 @@ package com.cbp.disponibilite.gestionnaires.manager;
 
 import com.cbp.disponibilite.gestionnaires.dao.GestionnaireDAO;
 import com.cbp.disponibilite.gestionnaires.model.entities.Gestionnaire;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,11 +13,16 @@ import java.util.List;
 @Service
 public class GestionnairesService {
 
+    private final static Logger LOG = LoggerFactory.getLogger(GestionnairesService.class);
+
     @Autowired
     GestionnaireDAO gestionnaireDAO;
 
+
     @Transactional
     public List<Gestionnaire> getGestionnaireByMatricule(final Long matricule) {
+        LOG.info("HELLOOOO");
+
         return gestionnaireDAO.findGestionnaire(matricule);
     }
 }
