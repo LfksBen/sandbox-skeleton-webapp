ff8b247ac6c988f162ac6b4d02f3d8a27bf84022
Benjamin Braud <benjamin.braud@cbp-group.com>
2019-01-17 16:24:23 +0100

    Test AWS

--- services/src/main/java/com/cbp/disponibilite/gestionnaires/services/resources/SupervisionResource.java
+++ services/src/main/java/com/cbp/disponibilite/gestionnaires/services/resources/SupervisionResource.java
@@ -5,7 +5,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
-@Path("/services/healthcheck")
+@Path("/healthcheck")
 public class SupervisionResource {
 
     @GET
